d727b9f40c07201725b8c279224abb71305c9b6e
Ale <alejandra_siles_a@hotmail.com>
2017-04-22 16:10:14 -0400

    1er intento correccion de falla

--- src/main/java/payrollcasestudy/transactions/change/ChangeNoMemberTransaction.java
+++ src/main/java/payrollcasestudy/transactions/change/ChangeNoMemberTransaction.java
@@ -1,15 +1,23 @@
 package payrollcasestudy.transactions.change;
 
+import static org.hamcrest.Matchers.nullValue;
+
+import payrollcasestudy.boundaries.PayrollDatabase;
+import payrollcasestudy.entities.Employee;
+import payrollcasestudy.entities.affiliations.UnionAffiliation;
 import payrollcasestudy.transactions.Transaction;
 
 public class ChangeNoMemberTransaction implements Transaction {
+	PayrollDatabase database = PayrollDatabase.globalPayrollDatabase;
+    private int employeeId;
 
 	public ChangeNoMemberTransaction(int employeeId) {
-		
+		this.employeeId = employeeId;
 	}
 
 	public void execute() {
-		
+		Employee employee = database.getEmployee(employeeId);
+        employee.setUnionAffiliation(null);
 	}
 
 }
